aa73eff05c732b0c31a29d42341d1709b2389224
Joseph Nuthalapati <njoseph@thoughtworks.com>
2018-02-23 18:03:50 +0530

    terraform configuration
    Signed-off-by: Joseph Nuthalapati <njoseph@thoughtworks.com>

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+resource "google_compute_instance" "raddit" {
+  name         = "not-raddit-instance"
+  machine_type = "n1-standard-1"
+  zone         = "asia-south1-a"
+
+  # boot disk specifications
+  boot_disk {
+    initialize_params {
+      image = "raddit-base" // use image built with Packer
+    }
+  }
+
+  # networks to attach to the VM
+  network_interface {
+    network = "default"
+    access_config {} // use ephemaral public IP
+  }
+}
+
---
+++ terraform/providers.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  version = "~> 1.4.0"
+  project = "josephsproject-196110"
+  region  = "asia-south1"
+}
